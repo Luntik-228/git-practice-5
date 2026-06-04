8ed301f5a363d2a27eb41e8071b9162eb73fea30
luntil-228 <borzenkov.stepan1106@gmail.com>
2026-06-04 22:39:32 +0300

    Добавлено второе изменение в main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
         System.out.println("Первое изменение в main");
+        System.out.println("Второе изменение в main");
     }
 }
