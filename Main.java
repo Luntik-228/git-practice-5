9cdcf74c005b5e6828cfe8a8d1cf68be61345f58
luntil-228 <borzenkov.stepan1106@gmail.com>
2026-06-04 22:39:52 +0300

    Добавлен метод printInfo

--- Main.java
+++ Main.java
@@ -3,5 +3,11 @@ class Main {
         System.out.println("Это добавлено в ветке main");
         System.out.println("Первое изменение в main");
         System.out.println("Второе изменение в main");
+
+        printInfo();
+    }
+
+    public static void printInfo() {
+        System.out.println("Метод printInfo выполнен");
     }
 }
